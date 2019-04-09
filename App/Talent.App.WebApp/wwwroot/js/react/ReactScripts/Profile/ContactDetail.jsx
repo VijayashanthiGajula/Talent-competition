d691ccc87170387de75aa11b6ea8fbf559fd2c11
VijayashanthiGajula <vijayashanthi.gajula@gmail.com>
2019-04-09 12:11:20 +1200

    c2

--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/ContactDetail.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/ContactDetail.jsx
@@ -10,6 +10,7 @@ export class IndividualDetailSection extends Component {
             Object.assign({}, props.details)
             : {
                 firstName: "",
+                lastName:"",
                 email: "",
                 phone: ""
             }
@@ -78,6 +79,16 @@ export class IndividualDetailSection extends Component {
                 />
                 <ChildSingleInput
                     inputType="text"
+                    label="Last Name"
+                    name="lastName"
+                    value={this.state.newContact.lastName}
+                    controlFunc={this.handleChange}
+                    maxLength={80}
+                    placeholder="Enter your last name"
+                    errorMessage="Please enter a valid first name"
+                />
+                <ChildSingleInput
+                    inputType="text"
                     label="Email address"
                     name="email"
                     value={this.state.newContact.email}
@@ -107,6 +118,7 @@ export class IndividualDetailSection extends Component {
     renderDisplay() {
 
         let firstName = this.props.details ? `${this.props.details.firstName}` : ""
+        let lastName = this.props.details ? `${this.props.details.lastName}`: ""
         let email = this.props.details ? this.props.details.email : ""
         let phone = this.props.details ? this.props.details.phone : ""
 
@@ -114,9 +126,10 @@ export class IndividualDetailSection extends Component {
             <div className='row'>
                 <div className="ui sixteen wide column">
                     <React.Fragment>
-                        <p>Name: {firstName}</p>
-                        <p>Email: {email}</p>
-                        <p>Phone: {phone}</p>
+                        <p>Name:{ firstName }<span>&nbsp;&nbsp;</span>{ lastName }</p>
+                        <p> Last Name:  {lastName}</p>
+                        <p>Email:   {email}</p>
+                        <p>Phone:   {phone}</p>
                     </React.Fragment>
                     <button type="button" className="ui right floated teal button" onClick={this.openEdit}>Edit</button>
                 </div>
@@ -251,7 +264,7 @@ export class CompanyDetailSection extends Component {
                         <p>Phone: {phone}</p>
                         <p> Location: {location.city}, {location.country}</p>
                     </React.Fragment>
-                    <button type="button" className="ui right floated teal button">Edit</button>
+                    <button type="button" className="ui right floated teal button" onClick={this.openEdit}>Edit</button>
                 </div>
             </div>
         )
